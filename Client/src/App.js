f1a7973f0abf749ded2e73b2ec31ae943ef6d2f4
Picowchew <58180935+Picowchew@users.noreply.github.com>
2021-06-26 16:58:37 -0400

    Linked sign up page to sign in page

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/login/login";
+import Signup from "./components/signup/signup";
 import Dashboard from "./components/dashboard/dashboard";
 import Marketplace from "./components/marketplace/marketplace";
 import Settings from "./components/settings/settings";
@@ -10,27 +11,26 @@ import Topbar from "./components/topbar/Topbar";
 import Menu from "./components/menu/Menu";
 import LandingPage from "./components/landing/landing";
 
-import "./app.scss"
+import "./app.scss";
 import { useState } from "react";
 
 function App() {
-
-  const [menuOpen,setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-
     <div className="extendPage">
       <Switch>
         <Route path="/" component={LandingPage} exact />
-        <Route path="/login" component={Login}/>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
         <div className="app">
-        <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-        <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/marketplace" component={Marketplace} />
-        <Route path="/settings" component={Settings} />
-        <Route path="/cart" component={Cart} />
-        <Route component={Other} />
+          <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/marketplace" component={Marketplace} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Other} />
         </div>
       </Switch>
     </div>
