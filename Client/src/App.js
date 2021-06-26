24bc3d6d2eefcc15d6c6da56b0b020b42bee490c
Picowchew <58180935+Picowchew@users.noreply.github.com>
2021-06-26 18:56:08 -0400

    Changed 'Dashboard' to 'Home'

--- Client/src/App.js
+++ Client/src/App.js
@@ -17,11 +17,9 @@ import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
 import Container from "@material-ui/core/Container";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import NotificationsIcon from "@material-ui/icons/Notifications";
 import { mainListItems, secondaryListItems } from "./listItems";
 
 function App() {
@@ -68,7 +66,7 @@ function App() {
                 noWrap
                 className={classes.title}
               >
-                Dashboard
+                Home
               </Typography>
             </Toolbar>
           </AppBar>
