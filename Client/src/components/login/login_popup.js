42fa9bf19f5c4c1329c394132a613c0cd805dcb1
Picowchew <58180935+Picowchew@users.noreply.github.com>
2021-06-26 11:23:38 -0400

    More login changes

--- Client/src/components/login/login_popup.js
+++ Client/src/components/login/login_popup.js
@@ -4,6 +4,7 @@ import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import "./login_popup.scss";
 
@@ -70,6 +71,11 @@ function LoginPopup() {
             OK
           </Button>
         </DialogActions>
+        <DialogContent>
+          <DialogContentText style={{ fontSize: 11 }}>
+            Don't have an account? Sign up!
+          </DialogContentText>
+        </DialogContent>
       </Dialog>
     </div>
   );
