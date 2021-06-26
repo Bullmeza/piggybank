0b98be66dee4bce9f72279b088dbd2808af10a48
unknown <robert.victor.muresan@gmail.com>
2021-06-26 13:47:34 -0400

    webscrape

--- Server/amazon/scrape.js
+++ Server/amazon/scrape.js
@@ -6,36 +6,31 @@ const fs = require('fs');
 
 const scrapeAmazon = async () => {
     let links = require('./input.json')
-    console.log(links)
     let productData = [];
 
-    links.forEach( async (link) => {
-        const response = await axios.get(link.link, {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
-            }
-        })
-        const $ = cheerio.load(response.data)
-        var price = $("#price_inside_buybox").text();
-        var image = $("#landingImage").attr('src')
-        var ASIN = link.link.match("/dp/(.*)/")
-        console.log(price, image, ASIN)
+    let promises = links.map( async (link) => {
+            const response = await axios.get(link.link, {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
+                }
+            })
+            const $ = cheerio.load(response.data)
+            var price = $("#price_inside_buybox").text();
+            var image = $("#landingImage").attr('src')
+            var ASIN = $('th:contains("ASIN") ~ ').text()
+            productData.push({
+                "name" : link.name.trim(),
+                "price" : price.trim(),
+                "image" : image.trim(),
+                "link" : link.link.trim(),
+                "ASIN" : ASIN.trim(),
+            })
     });
+    await Promise.all(promises);
 
-
-
+    fs.writeFile('./amazon/output.json', JSON.stringify(productData), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
 }
-
 module.exports = scrapeAmazon;
-// app.post("/getBarcodeInfo", async(req,res)=>{
-//     try{
-//         console.log(req.body)
-//         var response = await axios.get("https://www.amazon.ca/Nintendo-Switch-Animal-Crossing-Horizons/dp/B084DDDNRP/ref=sr_1_5?dchild=1&keywords=switch&qid=1624677941&sr=8-5")
-//         const $ = cheerio.load(response.data)
-//         var name = ""
-//         var img = $("#productTitle")
-//         res.status(200).send({name: name, image: img});
-//     } catch (err){
-//         res.status(404).send("Bad request");
-//     }
-// });
