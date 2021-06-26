75e8e6360c73d75cf7416f4b2850a7579fea81f0
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2021-06-26 16:01:38 -0400

    added redirect

--- Client/src/components/dashboard/dashboard.js
+++ Client/src/components/dashboard/dashboard.js
@@ -9,25 +9,21 @@ function Dashboard() {
   const cookie = document.cookie;
   var user;
   var money;
-  var loggedIn = true
 
   axios.post(`http://localhost:3001/validateSession_id`, {session_id: cookie.split("=")[1]}) //this link needs to be changed
   .then(res => {
     if (typeof res.data.username !== 'undefined') {
       user = res.data.username;
       money = res.data.money;
-      console.log(user, money)
     } else {
-      loggedIn = false
-      console.log(loggedIn)
+      window.location.replace("/");
     }
   });
 
+  console.log(loggedIn)
+
   return(
     <div>
-      <Route exact path="/">
-        {loggedIn ? this : <Redirect to="" />}
-      </Route>
       <div className="dashboard">
         <div className="title">Welcome to your Investment Dashboard!</div>
       </div>
